6a2f6454c1de7b1c4b60967071706c7970e4f2f4
shaggyshelar <shaggy.shelar@gmail.com>
2022-03-17 21:01:55 -0900

    Used pygame mixer for audio playing

--- SAI.py
+++ SAI.py
@@ -1,12 +1,17 @@
+#!/usr/bin/env python3
+
 import signal
 import RPi.GPIO as GPIO
 import sys
 import time
 import os
+import pygame
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 import subprocess
+from pygame import mixer
+
 import ST7789
 
 MESSAGE = "READY"
@@ -39,40 +44,35 @@ cwd = "{}/SAI-PYTHON-UI/".format(os.getcwd())
 cwd1 = os.getcwd()
 
 print("{}/Flite/test.wav".format(cwd))
-subprocess.run(['aplay',"{}/Flite/test.wav".format(cwd1)])
+mixer.init()
 
-# The buttons on Pirate Audio are connected to pins 5, 6, 16 and 24
-# Boards prior to 23 January 2020 used 5, 6, 16 and 20
-# try changing 24 to 20 if your Y button doesn't work.
+mixer.music.load("./Flite/test.wav")
+mixer.music.set_volume(0.7)
+mixer.music.play()
+
+a_sound = pygame.mixer.Sound("./Flite/A.wav")
+b_sound = pygame.mixer.Sound("./Flite/B.wav")
+x_sound = pygame.mixer.Sound("./Flite/X.wav")
+y_sound = pygame.mixer.Sound("./Flite/Y.wav")
+
 BUTTONS = [5, 6, 16, 24]
-
-# These correspond to buttons A, B, X and Y respectively
 LABELS = ['A', 'B', 'X', 'Y']
-
-# Set up RPi.GPIO with the "BCM" numbering scheme
 GPIO.setmode(GPIO.BCM)
-
-# Buttons connect to ground when pressed, so we should set them up
-# with a "PULL UP", which weakly pulls the input signal to 3.3V.
 GPIO.setup(BUTTONS, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-
-# "handle_button" will be called every time a button is pressed
-# It receives one argument: the associated input pin.
 def handle_button(pin):
     label = LABELS[BUTTONS.index(pin)]
     global MESSAGE
     MESSAGE  = "Button: {}".format(label)
-    # sound = AudioSegment.from_wav("{}/Flite/{}.wav".format(cwd,label))
-    # play(sound)
-    print("{}/Flite/{}.wav".format(cwd,label))
-    subprocess.run(['aplay',"{}/Flite/{}.wav".format(cwd1,label)])
+    if label == "A":
+        a_sound.play()
+    elif label == "B":
+        b_sound.play()
+    elif label == "X":
+        x_sound.play()
+    elif label == "Y":
+        y_sound.play()
     print("Button press detected on pin: {} label: {}".format(pin, label))
 
-
-# Loop through out buttons and attach the "handle_button" function to each
-# We're watching the "FALLING" edge (transition from 3.3V to Ground) and
-# picking a generous bouncetime of 100ms to smooth out button presses.
 for pin in BUTTONS:
     GPIO.add_event_detect(pin, GPIO.FALLING, handle_button, bouncetime=100)
 
@@ -83,9 +83,7 @@ HEIGHT = disp.height
 
 
 img = Image.new('RGB', (WIDTH, HEIGHT), color=(0, 0, 0))
-
 draw = ImageDraw.Draw(img)
-
 font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 30)
 
 title_size_x, title_size_y = draw.textsize(TITLE, font)
@@ -95,11 +93,9 @@ option1_size_x, option1_size_y = draw.textsize(OPTION1, font)
 option1_text_x = (disp.width - option1_size_x) // 2
 option1_text_y = title_size_y
 
-
 option2_size_x, option2_size_y = draw.textsize(OPTION2, font)
 option2_text_x = (disp.width - option2_size_x) // 2
 option2_text_y = title_size_y * 2
-
 
 option3_size_x, option3_size_y = draw.textsize(OPTION3, font)
 option3_text_x = (disp.width - option3_size_x) // 2
@@ -121,6 +117,4 @@ while True:
     draw.text((int(text_x - x), text_y), MESSAGE, font=font, fill=(255, 255, 255))
     disp.display(img)
 
-# Finally, since button handlers don't require a "while True" loop,
-# we pause the script to prevent it exiting immediately.
 # signal.pause()
