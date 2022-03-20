dc79ecf2af5c64cc7e1bb25b8ebc0818092fde24
Sagar Shelar <shaggy.shelar@gmail.com>
2022-03-20 15:10:25 +0530

    New change

--- rpiMenu/rpiMenu.py
+++ rpiMenu/rpiMenu.py
@@ -3,7 +3,7 @@ import pygame
 class Menu():
     def __init__(self, game):
         self.game = game
-        self.mid_w, self.mid_h = self.game.DISPLAY_W / 2, self.game.DISPLAY_H / 2
+        self.mid_w, self.mid_h = self.DISPLAY_W / 2, self.DISPLAY_H / 2
         self.run_display = True
         self.cursor_rect = pygame.Rect(0, 0, 20, 20)
         self.offset = - 100
