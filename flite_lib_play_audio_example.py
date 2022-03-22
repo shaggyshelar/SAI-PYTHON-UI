f4ec9cba60c15a716b24cd52dcd161a485e81a2c
Sagar Shelar <shaggy.shelar@gmail.com>
2022-03-22 13:59:23 +0530

    Completed in memory text to wave playing audio message

--- flite_lib_play_audio_example.py
+++ flite_lib_play_audio_example.py
@@ -1,23 +1,26 @@
 import fliteLib
 import pyaudio
 import wave
+import time
+p = pyaudio.PyAudio()
 
-waveOutput = fliteLib.textToWave("hi")
+waveOutput = fliteLib.textToWave("countries accepted India's position on conflict in Ukraine")
 print("**************** Output *********")
 # print(waveOutput)
 
 waveBytes = b"".join(waveOutput)
-print(waveBytes)
+#print(waveBytes)
 
 wf = wave.open("test.wav", 'rb')
-p = pyaudio.PyAudio()
-
 stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
                 channels=wf.getnchannels(),
                 rate=wf.getframerate(),
                 output=True)
 
 stream.write(waveBytes)
+time.sleep(5)
+
+print(" ******** Closing  *********")
 stream.stop_stream()
 stream.close()
 p.terminate()
